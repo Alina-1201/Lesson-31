65494ca38196e59aba016d005d866a2f9f4e8293
Alina-1201 <91902717+Alina-1201@users.noreply.github.com>
2022-01-17 19:54:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,324 @@
+// let a =prompt ('Введіть значення сторони а')
+// let p = a*4
+// alert (p)
+
+
+// let a = prompt ('введіть значення сторони а')
+// let s = a*a
+// alert(s)
+
+
+// let a = prompt ('Введіть значення сторони а')
+// let b = prompt ('Введіть значення сторони б')
+// let s = a*b
+// let p = a*2+b*2
+// alert(`S=${s}  P=${p}`)
+
+
+// let d = prompt ('Введіть значення діаметру')
+// let p = 3.14
+// let l = p*d
+// alert (l)
+
+
+// let a = prompt ('Введите значение длины ребра куба')
+// let v = a*a*a
+// let s = 6*a*a
+// alert(`V=${v} S=${s}`)
+
+
+// let a = prompt ('Введите значение ребра а')
+// let b = prompt ('Введите значение ребра в')
+// let c = prompt ('Введите значение ребра с')
+// let v = a*b*c
+// let s = 2*a*b + 2*b*c + 2*a*c
+// alert(`V=${v} S=${s}`)
+
+
+// let r = prompt ('Введіть значення радіусу')
+// let p = 3.14
+// let l = 2*p*r
+// let s = p*r*r
+// alert (`L=${l} S=${s}`)
+
+
+// let a = prompt ('Введіть значення а')
+// let b = prompt ('Введіть значення б')
+// let m = (a*1+b*1)/2
+// alert (m)
+
+
+// let a = prompt ('Введіть значення а')
+// let b = prompt ('Введіть значення б')
+// let m =  Math.sqrt(a*b)
+// alert (m)
+
+
+// let a = prompt ('Введіть значення числа а')
+// let b = prompt ('Введіть значення числа б')
+// let s = a*1+b*1
+// let r = a-b
+// let d = a*b
+// let m = Math.pow (a , 2)/ Math.pow(b , 2)
+// alert(s + '  ' + r + '  ' + d + '  ' + m)
+
+
+// let a = prompt ('Введіть значення числа а')
+// let b = prompt ('Введіть значення числа б')
+// let s = a*1+b*1
+// let r = a-b
+// let d = a*b
+// let m = Math.abs (a)/ Math.abs (b)
+// alert ( s + ' ' + r + ' ' + d + ' ' + m)
+
+
+// let a = prompt ('Введіть значення катета а')
+// let b = prompt ('Введіть значення катета б')
+// let c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2))
+// let p = a*1 + b*1 + c*1
+// alert(`c=${c} P=${p}`)
+
+
+// let r1 = prompt ('Введите значение R1, при R1>R2')
+// let r2 = prompt ('Введите значение R2, при R1>R2')
+// let p = 3.14
+// let s1 = p* Math.pow(r1,2)
+// let s2 = p* Math.pow (r2,2)
+// let s3 = s1*1-s2*1
+// alert(`S1=${s1} S2=${s2} S3=${s3}`)
+
+
+// let l = prompt ('Введіть довжину кола')
+// let p = 3.14
+// let r = l*1 / (p*2)
+// let s = p*Math.pow(r,2)
+// alert (`R=${r} S=${s}`)
+
+
+// let s = prompt ('Введіть значення площі')
+// let p = 3.14
+// let d = Math.sqrt(4*s / p*1)
+// let l = p*d
+// alert(`D=${d} L=${l}`)
+
+
+// let x1 = prompt ('Введіть координату точки')
+// let x2 = prompt ('Введіть координату точки')
+// let m = Math.abs(x1*1 - x2*1)
+// alert(m)
+
+
+// let a = prompt ('Введіть значення точки')
+// let b = prompt ('Введіть значення точки')
+// let c = prompt ('Введіть значення точки')
+// let d = a*1+c*1
+// let e = b*1+c*1
+// let s = d*1+e*1
+// alert(`AC=${d} BC=${e} AC+BC=${s}`)
+
+
+// let a = prompt ('A=')
+// let b = prompt ('B=')
+// let c = (a*1+b*1)/2
+// let s1 = c*1+a*1
+// let s2 = b*1+c*1
+// let s3 = s1*1+s2*1
+// alert (s3)
+
+
+// let x1 = prompt ('X1=')
+// let y1 = prompt ('Y1=')
+// let x2 = prompt ('X2=')
+// let y2 = prompt ('Y2=')
+// let k = Math.sqrt ((x2*1-x1*1) + (y2*1-y1*1))
+// let p = k*4
+// let s = Math.pow(k,2) 
+// alert (`P=${p} S=${s}`)
+
+
+// let x1 = prompt ('X1=')
+// let y1 = prompt ('Y1=')
+// let x2 = prompt ('X2=')
+// let y2 = prompt ('Y2=')
+// let l = Math.sqrt ((x2*1-x1*1) + (y2*1-y1*1))
+// alert (l)
+
+
+// let l = prompt ('Введіть довжину L в см')
+// let m = (l - l % 100) /100; 
+// alert (m)
+
+
+// let kg = prompt ('Введіть кількість кілограм')
+// let t = Math.floor (kg/1000)
+// alert (t)
+
+
+// let b = prompt ('Введіть кількість байтів')
+// let kb = Math.floor(b/1024)
+// alert (kb)
+
+
+// let a = prompt ('Введіть значення А, при А>В А>0')
+// let b = prompt ('Введіть значення B, при А>В В>0')
+// let k = Math.floor(a/b)
+// alert(k)s
+
+
+// let a = prompt ('Введіть значення А, при А>В А>0')
+// let b = prompt ('Введіть значення B, при А>В В>0')
+// let k = a%b
+// alert (k)
+
+
+// let cd = prompt ('Введіть двузначне число')
+// let a = Math.floor (cd/10)
+// let b = cd%10
+// alert (` Десятки - ${a} Одиниці - ${b}`)
+
+
+// let cd = prompt ('Введіть двузначне число')
+// let a = Math.floor (cd/10)
+// let b = cd%10
+// let s = a*1 + b*1
+// let m = a*b
+// alert (`Сума цифр = ${s} Добуток чисел = ${m}`)
+
+
+// let cd = prompt ('Введіть двузначне число')
+// let a = Math.floor (cd/10)
+// let b = cd%10
+// alert(`${b}${a}`)
+
+
+// let cde = prompt ('Введіть трьохзначне число')
+// let c = Math.floor (cde/100)
+// alert (c)
+
+
+// let a = prompt ('Введіть трьохзначне число')
+// let b = a%10
+// let c = Math.floor ((a/10)%10)
+// alert (`Одиниці - ${b} Деcятки - ${c}`)
+
+
+// let def = prompt ('Введіть трьохзначне число')
+// let a = def%10
+// let b = Math.floor ((def/10)%10)
+// let c = Math.floor (def/100)
+// let s = a*1+b*1+c*1
+// let m = a*b*c
+// alert(`${s} ${m}`)
+
+
+// let abc = prompt ('Введіть трьохзначне число')
+// let a = Math.floor (abc/100)
+// let b = Math.floor ((abc/10)%10)
+// let c = abc%10
+// alert (`${c}${b}${a}`)
+
+
+// let abc = prompt ('Введіть трьохзначне число')
+// let a = Math.floor (abc/100)
+// let b = Math.floor ((abc/10)%10)
+// let c = abc%10
+// alert(`${b}${c}${a}`)
+
+
+// let abc = prompt ('Введіть трьохзначне число')
+// let a = Math.floor (abc/100)
+// let b = Math.floor ((abc/10)%10)
+// let c = abc%10
+// alert (`${c}${a}${b}`)
+
+
+// let abc = prompt ('Введіть трьохзначне число')
+// let a = Math.floor (abc/100)
+// let b = Math.floor ((abc/10)%10)
+// let c = abc%10
+// alert(`${b}${a}${c}`)
+
+
+// let abc = prompt ('Введіть трьохзначне число')
+// let a = Math.floor (abc/100)
+// let b = Math.floor ((abc/10)%10)
+// let c = abc%10
+// alert(`${a}${c}${b}`)
+
+
+// let abc = prompt ('Введіть ціле число, a>999')
+// let dn = Math.floor(abc/1000)
+// let do = Math.floor ((abc/100)%10)
+// alert(`${do}  ${dn}`)
+
+
+// let abcd = prompt ('Введіть ціле число, a>999')
+// let a = Math.floor (abcd/1000)
+// alert(a)
+
+
+// let N = prompt ('Введіть кількість секунд, які пройшли з початку доби')
+// let min = Math.floor(N/60)
+// alert(min)
+
+
+// let N = prompt ('Введіть кількість секунд, які пройшли з початку доби')
+// let h = Math.floor (N/3600)
+// alert (h)
+
+
+// let N = prompt ('Введіть кількість секунд, які пройшли з початку доби')
+// let s = N%60
+// alert(s)
+
+
+// let N = prompt ('Введіть кількість секунд, які пройшли з початку доби')
+// let s = N%3600
+// alert(s)
+
+
+// let N = prompt ('Введіть кількість секунд, які пройшли з початку доби')
+// let min = Math.floor(N/60)
+// let h = Math.floor(min%60)
+// alert(h)
+
+
+// let k = prompt('Введіть ціле число К,  1-365')
+// let n = k%7
+// alert(n)
+
+
+// let k = prompt('Введіть ціле число К,  1-365')
+// let n = k%7+3
+// alert(n)
+
+
+// let k = prompt('Введіть ціле число К,  1-365')
+// let n = k%7+1
+// alert(n)
+
+
+// let k = prompt('Введіть ціле число К,  1-365')
+// let n = k%7+5
+// alert(n)
+
+
+// let k = prompt('Введіть ціле число К,  1-365')
+// let N = prompt('Введіть ціле число N,  1-7') 
+// let n = k%7+N*1
+// alert(n)
+
+
+// let a = prompt ('Введіть додатнє значення А')
+// let b = prompt ('Введіть додатнє значення В')
+// let c = prompt ('Введіть додатнє значення С')
+// let Sab = a*b
+// let Sc = c**2
+// let kc = Math.floor(Sab/Sc)
+// let nm = Sab-kc*Sc
+// alert(`Кількість квадратиків - ${kc} Незайняте місце - ${nm} `)
+
+
+// let a = prompt ('Введіть рік')
+// let b = Math.floor (a/100) + 1
+// alert (b)
